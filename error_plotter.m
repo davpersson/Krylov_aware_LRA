516fe9453356e85d23e40578ef10d1c9226192f3
davpersson <davper924@gmail.com>
2025-09-21 10:18:29 -0400

    plotter function update

--- error_plotter.m
+++ error_plotter.m
@@ -1,40 +1,28 @@
 function error_plotter(filename)
 
-load(filename)
+% Plots the results from error_test.m
 
-% semilogy((s_list + r_list)*k,optimal*ones(1,length(s_list)),'k','LineWidth',5)
-% hold on
-% semilogy((s_list + r_list)*k,krylov_aware_untruncated,'b--*','LineWidth',3)
-% semilogy((s_list + r_list)*k,krylov_aware_truncated,'b-*','LineWidth',3)
-% semilogy(2*s_list*k,randSVD_truncated,'r-*','LineWidth',3)
-% semilogy(2*s_list*k,exactrandSVD_truncated*ones(1,length(s_list)),'r--*','LineWidth',3)
-% legend({'Optimal','Krylov aware (untruncated)','Krylov aware (truncated)',...
-%     'randSVD','exact randSVD'},'location','best')
-% xlabel('$s=r$','Interpreter','latex')
-% ylabel('Relative Frobenius norm error','Interpreter','latex')
-% title_text = append('$k = $',num2str(k));
-% title(title_text,'interpreter','latex')
-% set(gca,'Fontsize',14)
-% hold off
+load(filename)
 
 semilogy(s_list,optimal*ones(1,length(s_list)),'k','LineWidth',5)
 hold on
-semilogy(s_list,krylov_aware_untruncated,'b--*','LineWidth',3)
-semilogy(s_list,krylov_aware_truncated,'b-+','LineWidth',3)
-semilogy(s_list,randSVD_truncated,'r-o','LineWidth',3)
-semilogy(s_list,exactrandSVD_truncated*ones(1,length(s_list)),'r--s','LineWidth',3)
+semilogy(s_list,krylov_aware_untruncated,'b--*','LineWidth',3,'MarkerSize', 12)
+semilogy(s_list,krylov_aware_truncated,'b-+','LineWidth',3,'MarkerSize', 12)
+semilogy(s_list,randSVD_truncated,'r-o','LineWidth',3,'MarkerSize', 12)
+semilogy(s_list,exactrandSVD_truncated*ones(1,length(s_list)),'r--s','LineWidth',3,'MarkerSize', 12)
 legend({'Optimal','Krylov aware (untruncated)','Krylov aware (truncated)',...
-    'randSVD','exact randSVD'},'location','best')
+    'randSVD','exact randSVD'},'location','best','Interpreter','latex')
 xlabel('$s=r$','Interpreter','latex')
 ylabel('Relative Frobenius norm error','Interpreter','latex')
 title_text = append('$k = $',num2str(k));
 title(title_text,'interpreter','latex')
 curtick = get(gca, 'xTick');
 xticks(unique(round(curtick)));
-set(gca,'Fontsize',16)
+set(gca,'Fontsize',18)
+set(gca,'TickLabelInterpreter','latex');
 hold off
 
-print(filename,'-depsc')
+print(filename,'-dpng')
 
 
 end
