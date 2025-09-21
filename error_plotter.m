78c43880d14dd6b93c817b87fba3b337ff3b295b
davpersson <davper924@gmail.com>
2025-09-21 10:33:32 -0400

    small edits to plotter functions

--- error_plotter.m
+++ error_plotter.m
@@ -6,9 +6,9 @@ load(filename)
 
 semilogy(s_list,optimal*ones(1,length(s_list)),'k','LineWidth',5)
 hold on
-semilogy(s_list,krylov_aware_untruncated,'b--*','LineWidth',3,'MarkerSize', 12)
+semilogy(s_list,krylov_aware_untruncated,'b--*','LineWidth',3,'MarkerSize', 15)
 semilogy(s_list,krylov_aware_truncated,'b-+','LineWidth',3,'MarkerSize', 12)
-semilogy(s_list,randSVD_truncated,'r-o','LineWidth',3,'MarkerSize', 12)
+semilogy(s_list,randSVD_truncated,'r-o','LineWidth',3,'MarkerSize', 15)
 semilogy(s_list,exactrandSVD_truncated*ones(1,length(s_list)),'r--s','LineWidth',3,'MarkerSize', 12)
 legend({'Optimal','Krylov aware (untruncated)','Krylov aware (truncated)',...
     'randSVD','exact randSVD'},'location','best','Interpreter','latex')
--- svk_error_plotter.m
+++ svk_error_plotter.m
@@ -7,11 +7,11 @@ load(filename)
 semilogy(2*k*s_list,optimal*ones(1,length(s_list)),'k','LineWidth',5)
 hold on
 semilogy(2*k*s_list,krylov_aware_untruncated,'b--*','LineWidth',3,'MarkerSize', 12)
-semilogy(2*k*s_list,krylov_aware_truncated,'b-+','LineWidth',3,'MarkerSize', 12)
+semilogy(2*k*s_list,krylov_aware_truncated,'b-+','LineWidth',3,'MarkerSize', 15)
 semilogy(2*k*s_list,svk_krylov_aware_untruncated,'m-|','LineWidth',3,'MarkerSize', 12)
-semilogy(2*k*s_list,svk_krylov_aware_truncated,'m--^','LineWidth',3,'MarkerSize', 12)
+semilogy(2*k*s_list,svk_krylov_aware_truncated,'m--^','LineWidth',3,'MarkerSize', 15)
 legend({'Optimal','Krylov aware (untruncated)','Krylov aware (truncated)',...
-    'Single vector Krylov aware (untruncated)','Single vector Krylov aware (truncated)'},'location','best')
+    'SV Krylov aware (untruncated)','SV Krylov aware (truncated)'},'location','northeast','interpreter','latex')
 xlabel('Matrix vector products with $\mathbf{A}$','Interpreter','latex')
 ylabel('Relative Frobenius norm error','Interpreter','latex')
 title_text = append('$k = $',num2str(k));
